e5a6b98c4a0c6901b1ea4a1a48940399e2dad165
Romina <rominchuela@gmail.com>
2023-07-17 20:03:40 -0400

    deploy

---
+++ js/about.318fd0a1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksesion9_firebase"]=self["webpackChunksesion9_firebase"]||[]).push([[443],{1272:function(s,e,n){n.r(e),n.d(e,{default:function(){return l}});var t=function(){var s=this;s._self._c;return s._m(0)},u=[function(){var s=this,e=s._self._c;return e("div",{staticClass:"about"},[e("h1",[s._v("This is an about page")])])}],i=n(1001),a={},r=(0,i.Z)(a,t,u,!1,null,null,null),l=r.exports}}]);
+//# sourceMappingURL=about.318fd0a1.js.map
